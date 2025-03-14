3e62c971e854b7459432d7689499b30da89d7be3
Muhammad Abdullah <testuserforprogramming@gmail.com>
2025-03-14 15:37:22 +0500

    Replaced MagicCalendar.kt with MagicCalendar.java
    Speed issue is only pending issue which will be resolved in next commit

---
+++ MagicCalendar/src/main/java/com/custom/magic/calendar/MagicCalendar.java
@@ -0,0 +1,10 @@
+package com.custom.magic.calendar;
+
+import android.app.Application;
+import com.jakewharton.threetenabp.AndroidThreeTen;
+
+public class MagicCalendar {
+    public static void init(Application application){
+        AndroidThreeTen.init(application);
+    }
+}
